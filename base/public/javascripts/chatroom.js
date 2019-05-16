0315d5207b147a2fac8c13d2f9544a8e1ca7ddfc
C-N-G <cormac_gudge@hotmail.com>
2019-05-16 13:12:47 +0100

    Added user is typing functionality

--- base/public/javascripts/chatroom.js
+++ base/public/javascripts/chatroom.js
@@ -12,6 +12,14 @@ $( document ).ready(function() {
         $('#m').val('');
         return false;
     });
+    $('#m').on('input', function(){
+        if ($('#m').val()){
+            socket.emit('user is typing', 'dummy');
+        }
+        else {
+            socket.emit('user is not typing', 'dummy');
+        }
+    });
     $('#nick-bar').submit(function(e){
         e.preventDefault(); // prevents page reloading
         if ($('#nick').val()) {
@@ -44,5 +52,26 @@ $( document ).ready(function() {
         for (var i = 0; i < data.length; i++) {
             $('#clients').append($('<li>').text(data[i]));
         }
+    });
+    socket.on('user is typing', function(msg){
+        var data = JSON.parse(msg);
+        var id_found = false;
+        $('#typing').children().each(function(){
+            if ($(this).attr('data-id') == data.nickname_id){
+                id_found = true;
+            }
+        });
+        if (id_found == false){
+            $('#typing').append('<li data-id="' + data.nickname_id + '">' + data.nickname + ' is typing...</li>');
+        }
+
+    });
+    socket.on('user is not typing', function(msg){
+        var data = JSON.parse(msg);
+        $('#typing').children().each(function(){
+            if ($(this).attr('data-id') == data.nickname_id){
+                $(this).remove();
+            }
+        });
     });
 });
