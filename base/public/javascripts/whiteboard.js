5ad35d892b696b912f94681ab1ba792b46e6e121
C-N-G <cormac_gudge@hotmail.com>
2019-05-18 13:55:58 +0100

    added timing to drawing on whiteboard

--- base/public/javascripts/whiteboard.js
+++ base/public/javascripts/whiteboard.js
@@ -9,57 +9,129 @@ $( document ).ready(function(){
     var draw_size = 6;
 
     var draw = $('#drawing_area')[0].getContext('2d');
-    draw.strokeStyle = '#000000';
+    //draw.strokeStyle = '#000000';
     draw.lineWidth = draw_size;
     draw.lineCap = 'round';
     draw.lineJoin = "round";
-    var draw_1 = $('#drawing_area')[0].getContext('2d');
-    draw_1.strokeStyle = '#000000';
-    draw_1.lineWidth = draw_size;
-    draw_1.lineCap = 'round';
-    draw_1.lineJoin = "round";
 
-    
+    var last_pos = {'x' : 0, 'y' : 0,};
+    var last_net_pos = {'x' : 0, 'y' : 0,};
 
-    function mouse_draw() {
-        if (mouse_click && !mouse_drawing) {
+    var inputs = [];
+    var net_inputs = [];
+
+    setInterval(start_draw_loop, 20);
+    setTimeout(setInterval(start_net_draw_loop, 20), 10);
+
+
+    function store_inputs(x, y, clicked, drawing) {
+        inputs.push({'x': x, 'y': y, 'clicked': clicked, 'drawing': drawing});
+    }
+
+    function net_store_inputs(x, y, clicked, drawing) {
+        net_inputs.push({'x': x, 'y': y, 'clicked': clicked, 'drawing': drawing});
+    }
+    //
+    function start_draw_loop() {
+        draw.strokeStyle = '#000000';
+        if (inputs.length > 0) {
+            for (var i = 0; i < inputs.length; i++) {
+                mouse_draw_real(inputs[i].x, inputs[i].y, inputs[i].clicked, inputs[i].drawing);
+            }
+            inputs = [];
+        }
+    }
+
+    function start_net_draw_loop() {
+        draw.strokeStyle = '#FF0000';
+        if (net_inputs.length > 0) {
+            for (var i = 0; i < net_inputs.length; i++) {
+                mouse_draw_real(net_inputs[i].x, net_inputs[i].y, net_inputs[i].clicked, net_inputs[i].drawing);
+            }
+            net_inputs = [];
+        }
+    }
+
+    function mouse_draw_real(x, y, clicked, drawing) {
+        if (clicked && !drawing) {
 
             draw.beginPath();
-            draw.arc(mouse_x, mouse_y, (draw_size / 2), 0, 2 * Math.PI);
+            draw.arc(x, y, (draw_size / 2), 0, 2 * Math.PI);
             draw.fill();
 
             draw.beginPath();
-            draw.lineTo(mouse_x, mouse_y);
+            draw.lineTo(x, y);
 
+        } else if (clicked && drawing) {
+
+            draw.lineTo(x, y);
+            draw.stroke();
+
+        } else if (!clicked) {
+
+        }
+    }
+
+    function mouse_draw() {
+        if (mouse_click && !mouse_drawing) {
+            store_inputs(mouse_x, mouse_y, mouse_click, mouse_drawing);
             mouse_drawing = true;
             socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': true, 'draw': false});
         } else if (mouse_click && mouse_drawing) {
-            draw.lineTo(mouse_x, mouse_y);
-            draw.stroke();
             socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': true, 'draw': true});
+            store_inputs(mouse_x, mouse_y, mouse_click, mouse_drawing);
         } else if (!mouse_click) {
             if (mouse_drawing){
                 socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': false, 'draw': false});
+                store_inputs(mouse_x, mouse_y, mouse_click, mouse_drawing);
             }
             mouse_drawing = false;
         }
     }
-    function net_mouse_draw(net_mouse_x, net_mouse_y, net_click, net_draw) {
-        if (net_click && !net_draw) {
 
-            draw_1.beginPath();
-            draw_1.arc(net_mouse_x, net_mouse_y, (draw_size / 2), 0, 2 * Math.PI);
-            draw_1.fill();
+    // function net_mouse_draw_real(net_mouse_x, net_mouse_y, net_click, net_draw) {
+    //     if (net_click && !net_draw) {
+    //
+    //         draw.beginPath();
+    //         draw.arc(net_mouse_x, net_mouse_y, (draw_size / 2), 0, 2 * Math.PI);
+    //         draw.fill();
+    //
+    //         draw.beginPath();
+    //         draw.lineTo(net_mouse_x, net_mouse_y);
+    //
+    //     } else if (net_click && net_draw) {
+    //
+    //         draw.lineTo(net_mouse_x, net_mouse_y);
+    //         draw.stroke();
+    //
+    //     } else if (!net_click) {
+    //
+    //     }
+    // }
 
-            draw_1.beginPath();
-            draw_1.lineTo(net_mouse_x, net_mouse_y);
-        } else if (net_click && net_draw) {
-            draw_1.lineTo(net_mouse_x, net_mouse_y);
-            draw_1.stroke();
-        } else if (!net_click) {
-
-        }
-    }
+    // function mouse_draw(x, y, clicked, drawing) {
+    //     if (mouse_click && !mouse_drawing) {
+    //
+    //         draw.beginPath();
+    //         draw.arc(mouse_x, mouse_y, (draw_size / 2), 0, 2 * Math.PI);
+    //         draw.fill();
+    //
+    //         draw.beginPath();
+    //         draw.lineTo(mouse_x, mouse_y);
+    //
+    //         mouse_drawing = true;
+    //         socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': true, 'draw': false});
+    //     } else if (mouse_click && mouse_drawing) {
+    //         draw.lineTo(mouse_x, mouse_y);
+    //         draw.stroke();
+    //         socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': true, 'draw': true});
+    //     } else if (!mouse_click) {
+    //         if (mouse_drawing){
+    //             socket.emit('draw', {'cord_x': mouse_x, 'cord_y': mouse_y, 'click': false, 'draw': false});
+    //         }
+    //         mouse_drawing = false;
+    //     }
+    // }
     /*
         TODO:
         add heart beat so that every 20ms it switches between
@@ -71,17 +143,20 @@ $( document ).ready(function(){
     $('#drawing_area').on('mousedown', function(){
         mouse_click = true;
         mouse_draw();
+        //store_inputs(mouse_x, mouse_y, mouse_clicked, mouse_drawing);
     }).on('mousemove', function(event){
         offset = $('#drawing_area').offset();
         mouse_x = (event.pageX - offset.left);
         mouse_y = (event.pageY - offset.top);
         $('.testing').text('X = '+ mouse_x + ', Y = ' + mouse_y);
         mouse_draw();
+        //store_inputs(mouse_x, mouse_y, mouse_clicked, mouse_drawing);
     }).on('mouseup', function(){
         mouse_click = false;
     }).on('mouseleave', function(){
         mouse_click = false;
         mouse_draw();
+        //store_inputs(mouse_x, mouse_y, mouse_clicked, mouse_drawing);
     });
 
     socket.on('draw', function(data){
@@ -89,6 +164,6 @@ $( document ).ready(function(){
             console.log('someone is drawing');
             console.log(data);
         }
-        net_mouse_draw(data.cord_x, data.cord_y, data.click, data.draw)
+        net_store_inputs(data.cord_x, data.cord_y, data.click, data.draw);
     })
 });
