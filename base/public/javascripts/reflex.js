db57f37d1d4d4c8494193c96ab535bcd0ca3d3e6
C-N-G <cormac_gudge@hotmail.com>
2019-06-01 21:01:43 +0100

    Added collision logic

--- base/public/javascripts/reflex.js
+++ base/public/javascripts/reflex.js
@@ -19,7 +19,7 @@ $( document ).ready(function(){
 
     // Resize the stage
     app.renderer.autoDensity = true;
-    app.renderer.resize(512,512);
+    app.renderer.resize(768,768);
 
     // Resize stage to screen size
     // app.renderer.view.style.position = "absolute";
@@ -52,6 +52,7 @@ $( document ).ready(function(){
 
     let state, player, foe, gravity, speed, cooldown_off;
     var projectiles = [];
+    var foes = [];
     //Capture the keyboard arrow keys
     let left = keyboard("ArrowLeft"),
         up = keyboard("ArrowUp"),
@@ -65,24 +66,13 @@ $( document ).ready(function(){
 
     function setup() {
         console.log('setup')
-        //let player = new sprite( resources.player.texture );
-        //let tileset = new sprite(resources['images/reflex.png'].texture);
 
-        player = new sprite(texture_cache['player.png']);
-        player.position.set(256, 256);
-        player.anchor.x = 0.5;
-        player.anchor.y = 0.5;
-        player.vx = 0;
-        player.vy = 0;
-        app.stage.addChild(player);
+        create_player('player.png', 256, 256, 11, 0.5, 10, 30)
 
-        foe = new sprite(texture_cache['foe.png']);
-        foe.position.set(256, 32);
-        foe.anchor.x = 0.5;
-        foe.anchor.y = 0.5;
-        foe.vx = 0;
-        foe.vy = 0;
-        app.stage.addChild(foe);
+        create_foe('foe.png', 100, 32, 6, 0.2, 4, 600)
+        create_foe('foe.png', 200, 32, 6, 0.2, 4, 600)
+        create_foe('foe.png', 300, 32, 6, 0.2, 4, 600)
+        create_foe('foe.png', 400, 32, 6, 0.2, 4, 600)
 
         $(window).on('mousemove', function(event){
             mouse.x = event.pageX;
@@ -112,8 +102,7 @@ $( document ).ready(function(){
         // app.stage.removeChild(anySprite)
         // anySprite.visible = false;
         state = play;
-        cooldown_off = true;
-        //console.log(app.stage);
+
         app.ticker.add(delta => game_loop(delta));
 
     }
@@ -125,57 +114,229 @@ $( document ).ready(function(){
     function play(delta){
 
         gravity = 0.95;
-        speed = 0.5;
-        top_speed = 10
         stop_point = 0.1
 
-        if (left.isDown == true) { player.vx -= speed }
-        if (right.isDown == true) { player.vx += speed }
-        if (up.isDown == true) { player.vy -= speed }
-        if (down.isDown == true) { player.vy += speed }
+        player_update();
+        foe_update();
+        projectile_update();
+
+    }
+    function projectile_update() {
+        
+        let projectiles_remove = [];
+
+        for (var i = 0; i < projectiles.length; i++) {
+            projectiles[i].x += Math.cos(projectiles[i].rotation - (Math.PI / 2)) * projectiles[i].speed;
+            projectiles[i].y += Math.sin(projectiles[i].rotation - (Math.PI / 2)) * projectiles[i].speed;
+
+            keep_in(projectiles[i], true);
+
+            for (var ii = 0; ii < projectiles.length; ii++) {
+                if (collision_check(projectiles[i], projectiles[ii]) && projectiles[i].team != projectiles[ii].team) {
+                    projectiles[i].alive = false;
+                    projectiles[ii].alive = false;
+                }
+            }
+
+            if (projectiles[i].alive == false) {
+                projectiles[i].visible = false;
+                projectiles_remove.push(i);
+            }
+        }
+
+        for (var i = (projectiles_remove.length - 1); i > -1; i--) {
+            projectiles.splice(projectiles_remove[i], 1);
+        }
+
+    }
+
+    function player_update() {
+
+        if (left.isDown == true) { player.vx -= player.acceleration_speed }
+        if (right.isDown == true) { player.vx += player.acceleration_speed }
+        if (up.isDown == true) { player.vy -= player.acceleration_speed }
+        if (down.isDown == true) { player.vy += player.acceleration_speed }
 
         player.vy *= gravity;
         player.vx *= gravity;
 
         if (player.vx < stop_point && player.vx > -stop_point) { player.vx = 0 };
-        player.vx = (player.vx > top_speed) ? top_speed : player.vx;
-        player.vx = (player.vx < -top_speed) ? -top_speed : player.vx;
+        player.vx = (player.vx > player.top_speed) ? player.top_speed : player.vx;
+        player.vx = (player.vx < -player.top_speed) ? -player.top_speed : player.vx;
 
         if (player.vy < stop_point && player.vy > -stop_point) { player.vy = 0 };
-        player.vy = (player.vy > top_speed) ? top_speed : player.vy;
-        player.vy = (player.vy < -top_speed) ? -top_speed : player.vy;
+        player.vy = (player.vy > player.top_speed) ? player.top_speed: player.vy;
+        player.vy = (player.vy < -player.top_speed) ? -player.top_speed : player.vy;
 
-        let adjacent = player.y - mouse.y,
-            opposite = player.x - mouse.x,
-            hypotenuse = Math.sqrt((adjacent * adjacent) + (opposite * opposite));
-            angle = Math.asin(opposite / hypotenuse);
-        player.rotation = (adjacent >= 0) ? -angle : Math.PI + angle;
+        player.rotation = Math.atan2(mouse.x - player.x, player.y - mouse.y);
 
-        if (mouse.pressed && cooldown_off) {
-            shoot(player.x, player.y, player.rotation);
-            cooldown_off = false;
-            setTimeout(function(){ cooldown_off = true; }, 250);
+        if (mouse.pressed && player.shoot && player.alive) {
+            create_projectile(player);
+            player.shoot = false;
         }
-        //console.log(adjacent + ' | ' + opposite + ' | ' + hypotenuse + ' | ' + angle);
-        //console.log(mouse.pressed);
-        player.y += player.vy;
-        player.x += player.vx;
+
+        if (!player.shoot) {
+            player.projectile_cooldown--;
+        }
+
+        if (player.projectile_cooldown <= 0) {
+            player.projectile_cooldown = player.projectile_cooldown_reset;
+            player.shoot = true;
+        }
+
         for (var i = 0; i < projectiles.length; i++) {
-            projectiles[i].x += Math.cos(projectiles[i].rotation - (Math.PI / 2))*projectiles[i].speed;
-            projectiles[i].y += Math.sin(projectiles[i].rotation - (Math.PI / 2))*projectiles[i].speed;
+            if (collision_check(projectiles[i], player) && projectiles[i].team != player.team) {
+                player.alive = false;
+                projectiles[i].alive = false;
+            }
         }
-        //console.log(player.vx + ' | ' + player.vy);
-        //console.log(app.renderer.width + ' Z ' + app.renderer.height);
+
+        if (player.alive) {
+            player.x += player.vx;
+            player.y += player.vy;
+        } else if (player.alive == false) {
+            player.visible = false;
+        }
+
+        keep_in(player, false);
     }
-    function shoot(x, y, rotation) {
+
+    function foe_update() {
+
+        for (var i = 0; i < foes.length; i++) {
+            foes[i].rotation = Math.atan2(player.x - foes[i].x, foes[i].y - player.y);
+
+            if (foes[i].alive && foes[i].shoot) {
+                create_projectile(foes[i]);
+                foes[i].shoot = false;
+            }
+
+            foes[i].projectile_cooldown -= (!foes[i].shoot) ? random_int(10) : 0;
+
+            if (foes[i].projectile_cooldown <= 0) {
+                foes[i].projectile_cooldown = foes[i].projectile_cooldown_reset;
+                foes[i].shoot = true;
+            }
+
+            if (collision_check(foes[i], player)) {
+                player.alive = false;
+            }
+
+            for (var ii = 0; ii < projectiles.length; ii++) {
+                if (collision_check(projectiles[ii], foes[i]) && projectiles[ii].team != foes[i].team) {
+                    foes[i].alive = false;
+                    projectiles[ii].alive = false;
+                }
+            }
+
+            if (foes[i].alive == false) {
+                foes[i].visible = false;
+            }
+        }
+
+    }
+
+    function collision_check(obj1, obj2) {
+        if (
+            (((obj1.x + (obj1.hitbox_size / 2)) >= (obj2.x - (obj2.hitbox_size / 2)) && (obj1.x + (obj1.hitbox_size / 2)) <= (obj2.x + (obj2.hitbox_size / 2)))
+            ||
+            ((obj1.x - (obj1.hitbox_size / 2)) <= (obj2.x + (obj2.hitbox_size / 2)) && (obj1.x - (obj1.hitbox_size / 2)) >= (obj2.x - (obj2.hitbox_size / 2))))
+            &&
+            (((obj1.y + (obj1.hitbox_size / 2)) >= (obj2.y - (obj2.hitbox_size / 2)) && (obj1.y + (obj1.hitbox_size / 2)) <= (obj2.y + (obj2.hitbox_size / 2)))
+            ||
+            ((obj1.y - (obj1.hitbox_size / 2)) <= (obj2.y + (obj2.hitbox_size / 2)) && (obj1.y - (obj1.hitbox_size / 2)) >= (obj2.y - (obj2.hitbox_size / 2))))
+            &&
+            (obj1.alive == true && obj2.alive == true)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function keep_in(obj, kill) {
+        let x_bounds = app.renderer.width,
+            y_bounds = app.renderer.height;
+        if (kill == false) {
+            if (obj.x + (obj.hitbox_size / 2) > x_bounds) {
+                obj.x = x_bounds - (obj.hitbox_size / 2);
+                obj.vx = 0;
+            } else if (obj.x - (obj.hitbox_size / 2) < 0) {
+                obj.x = (obj.hitbox_size / 2);
+                obj.vx = 0;
+            }
+
+            if (obj.y + (obj.hitbox_size / 2) > y_bounds) {
+                obj.y = y_bounds - (obj.hitbox_size / 2);
+                obj.vy = 0;
+            } else if (obj.y - (obj.hitbox_size / 2) < 0) {
+                obj.y = (obj.hitbox_size / 2);
+                obj.vy = 0;
+            }
+        } else if (kill == true) {
+            if (
+                obj.x + (obj.hitbox_size / 2) > x_bounds ||
+                obj.x - (obj.hitbox_size / 2) < 0 ||
+                obj.y + (obj.hitbox_size / 2) > y_bounds ||
+                obj.y - (obj.hitbox_size / 2) < 0
+            ) {
+                obj.alive = false;
+            }
+        }
+
+    }
+
+    function create_projectile(obj) {
         projectile = new sprite(texture_cache['fireball.png'])
-        projectile.position.set(x,y);
-        projectile.rotation = rotation;
+        projectile.position.set(obj.x, obj.y);
+        projectile.team = obj.team;
+        projectile.rotation = obj.rotation;
+        projectile.hitbox_size = 16;
         projectile.anchor.x = 0.5;
         projectile.anchor.y = 0.5;
-        projectile.speed = 11;
+        projectile.alive = true;
+        projectile.speed = obj.projectile_speed;
         app.stage.addChild(projectile);
         projectiles.push(projectile);
+    }
+
+    function create_player(file, x_start, y_start, projectile_speed, acceleration_speed, top_speed, shoot_speed) {
+        player = new sprite(texture_cache[file]);
+        player.position.set(x_start, y_start);
+        player.team = 1;
+        player.hitbox_size = 22;
+        player.anchor.x = 0.5;
+        player.anchor.y = 0.5;
+        player.vx = 0;
+        player.vy = 0;
+        player.alive = true;
+        player.shoot = true;
+        player.projectile_speed = projectile_speed;
+        player.projectile_cooldown = shoot_speed;
+        player.projectile_cooldown_reset = shoot_speed;
+        player.acceleration_speed = acceleration_speed;
+        player.top_speed = top_speed;
+        app.stage.addChild(player);
+    }
+
+    function create_foe(file, x_start, y_start, projectile_speed, acceleration_speed, top_speed, shoot_speed) {
+        foe = new sprite(texture_cache[file]);
+        foe.position.set(x_start, y_start);
+        foe.team = 2;
+        foe.hitbox_size = 32;
+        foe.anchor.x = 0.5;
+        foe.anchor.y = 0.5;
+        foe.vx = 0;
+        foe.vy = 0;
+        foe.alive = true;
+        foe.shoot = true;
+        foe.projectile_speed = projectile_speed;
+        foe.projectile_cooldown = shoot_speed;
+        foe.projectile_cooldown_reset = shoot_speed;
+        foe.acceleration_speed = acceleration_speed;
+        foe.top_speed = top_speed;
+        foes.push(foe);
+        app.stage.addChild(foe);
     }
 
     function keyboard(value) {
@@ -223,6 +384,10 @@ $( document ).ready(function(){
       };
 
       return key;
+    }
+
+    function random_int(max) {
+        return Math.floor((Math.random() * Math.floor(max)) + 1);
     }
 
 })
