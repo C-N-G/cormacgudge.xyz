411e1122bd4c2375e46df46686e9bb5a98bf7c96
C-N-G <cormac_gudge@hotmail.com>
2019-12-27 08:56:04 +0000

    Updated landing page

--- base/routes/index.js
+++ base/routes/index.js
@@ -1,24 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var joker_controller = require('../controllers/joker');
-var chatroom_controller = require('../controllers/chatroom');
-var whiteboard_controller = require('../controllers/whiteboard');
-var reflex_controller = require('../controllers/reflex');
-var icarus_controller = require('../controllers/icarus');
-var battleship_controller = require('../controllers/battleship');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {title: 'Frontdoor'});
 });
-router.get('/joker', joker_controller.render);
+
+var joker_controller = require('../controllers/joker');             router.get('/joker', joker_controller.render);
+
 router.get('/joker_result_type1', function(req, res, next) {
   res.render('joker_result_type1');
 });
-router.get('/chatroom', chatroom_controller.render);
-router.get('/whiteboard', whiteboard_controller.render);
-router.get('/reflex', reflex_controller.render);
-router.get('/icarus', icarus_controller.render);
-router.get('/battleship', battleship_controller.render);
+
+var chatroom_controller = require('../controllers/chatroom');       router.get('/chatroom', chatroom_controller.render);
+var whiteboard_controller = require('../controllers/whiteboard');   router.get('/whiteboard', whiteboard_controller.render);
+var reflex_controller = require('../controllers/reflex');           router.get('/reflex', reflex_controller.render);
+var icarus_controller = require('../controllers/icarus');           router.get('/icarus', icarus_controller.render);
+var battleship_controller = require('../controllers/battleship');   router.get('/battleship', battleship_controller.render);
+
 
 module.exports = router;
