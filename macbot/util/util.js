581b1733543dfd9415db685432170e30f2ae31c7
C-N-G <cormac_gudge@hotmail.com>
2020-05-04 02:21:49 +0100

    macbot: fixed queue timestamp typo

--- macbot/util/util.js
+++ macbot/util/util.js
@@ -6,7 +6,7 @@ module.exports = {
 
     let minutes = Math.floor(seconds/60);
     let returnSeconds = seconds - minutes*60;
-    if (returnSeconds <= 10) returnSeconds = '0' + returnSeconds;
+    if (returnSeconds < 10) returnSeconds = '0' + returnSeconds;
 
     if (minutes < 60) {
       return `${minutes}:${returnSeconds}`;
@@ -14,7 +14,7 @@ module.exports = {
 
     let hours = Math.floor(seconds/60/60);
     minutes -= hours*60;
-    if (minutes <= 10) minutes = '0' + minutes;
+    if (minutes < 10) minutes = '0' + minutes;
     return `${hours}:${minutes}:${returnSeconds}`;
   },
 
